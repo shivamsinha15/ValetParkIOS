688565e5f4cb4242037cc29a876a05d5bb67903c
Shivam Sinha <shivam_15@hotmail.com>
2014-01-28 10:09:38 +1100

    Initial Commit

---
+++ ValetParkTests/ValetParkTests.m
@@ -0,0 +1,34 @@
+//
+//  ValetParkTests.m
+//  ValetParkTests
+//
+//  Created by Shivam Sinha on 28/01/2014.
+//  Copyright (c) 2014 SaxStudios. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ValetParkTests : XCTestCase
+
+@end
+
+@implementation ValetParkTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
